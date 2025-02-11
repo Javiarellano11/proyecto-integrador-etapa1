f8776a0eba2e78cd0c59d0b598543547e8d82862
Javier Arellano <javi.arellano@outlook.com>
2025-02-11 19:54:21 -0300

    generé las card dinamicamente, cree la carpeta db y el archivo productos.js

---
+++ src/db/productos.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Botines Nike AirZoom Vapor 16 Pro Fg",
+        foto: "imgs/nike-airzoom-vapor.webp",
+        descripcion: "botines nike azul y celeste"
+    },
+    {
+        id: 2,
+        nombre: "Botines Nike Tiempo Legend 10 Academy",
+        foto: "imgs/nike-tiempo-legend.webp",
+        descripcion: "botines nike tiempo de color salmon"
+    },
+    {
+        id: 3,
+        nombre: "Botines adidas F50 League Fg",
+        foto: "imgs/botines-adidas-f50-blanco.webp",
+        descripcion: "botines adidas blanco, azul y rojo f50"
+    },
+    {
+        id: 4,
+        nombre: "Botines adidas Predator Accuracy.2 Fg",
+        foto: "imgs/botines-adidas-predator-negro-rosa.webp",
+        descripcion: "botines adidas predator negro y rosa"
+    },
+    {
+        id: 5,
+        nombre: "Botines Puma Future 7 Pro Fg/Ag",
+        foto: "imgs/botines-puma-rojo.webp",
+        descripcion: "botines puma rojo"
+    },
+]
+
+export default productos
