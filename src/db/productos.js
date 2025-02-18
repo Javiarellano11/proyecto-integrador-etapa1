865b82dbaf3c670e3ecbf57150805be513490529
Javier Arellano <javi.arellano@outlook.com>
2025-02-18 00:17:02 -0300

    modifico el buscador, agrego el logo de la pagina y el footer con sus logos de pagos

--- src/db/productos.js
+++ src/db/productos.js
@@ -3,31 +3,31 @@ const productos = [
         id: 1,
         nombre: "Botines Nike AirZoom Vapor 16 Pro Fg",
         foto: "imgs/nike-airzoom-vapor.webp",
-        descripcion: "botines nike azul y celeste"
+        descripcion: "Los Nike Air Zoom Vapor 16 Pro FG son botines de fútbol ligeros y de alto rendimiento, diseñados para jugadores rápidos."
     },
     {
         id: 2,
         nombre: "Botines Nike Tiempo Legend 10 Academy",
         foto: "imgs/nike-tiempo-legend.webp",
-        descripcion: "botines nike tiempo de color salmon"
+        descripcion: "El cuero con diseño avanzado FlyTouch Lite es increíblemente suave. Ayuda a que el calzado se adapte al pie para ofrecer mayor comodidad sin exceso de elasticidad."
     },
     {
         id: 3,
         nombre: "Botines adidas F50 League Fg",
         foto: "imgs/botines-adidas-f50-blanco.webp",
-        descripcion: "botines adidas blanco, azul y rojo f50"
+        descripcion: "Los Adidas F50 League FG son botines ligeros y de alto rendimiento, diseñados para jugadores que buscan velocidad y control."
     },
     {
         id: 4,
         nombre: "Botines adidas Predator Accuracy.2 Fg",
         foto: "imgs/botines-adidas-predator-negro-rosa.webp",
-        descripcion: "botines adidas predator negro y rosa"
+        descripcion: "El exterior HybridTouch suave de estos botines incorpora un cuello de corte medio en material textil para un ajuste perfecto"
     },
     {
         id: 5,
         nombre: "Botines Puma Future 7 Pro Fg/Ag",
         foto: "imgs/botines-puma-rojo.webp",
-        descripcion: "botines puma rojo"
+        descripcion: "El Dynamic Motion System otorga tracción y agilidad para movimientos rápidos y cambios de velocidad. Cuando te vistes de FUTURE, eres el futuro."
     },
 ]
 
